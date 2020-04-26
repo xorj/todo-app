76cc5e4985666adbf929ffeac44dbafa919f29e4
xorj <jorge.santos@dcomp.ufs.br>
2020-04-26 20:38:27 -0300

    Updates

---
+++ precache-manifest.e8a054decf29c41258536b8f9c559e4d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6019b35a5f6219a7eda8300d8ddec239",
+    "url": "/todo-app/index.html"
+  },
+  {
+    "revision": "0271c92388d8844123ce",
+    "url": "/todo-app/static/css/main.dffc5d68.chunk.css"
+  },
+  {
+    "revision": "59a1acb59e8b98329e12",
+    "url": "/todo-app/static/js/2.662c3396.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/todo-app/static/js/2.662c3396.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0271c92388d8844123ce",
+    "url": "/todo-app/static/js/main.d4bb87e1.chunk.js"
+  },
+  {
+    "revision": "8c574afb4aca7e7fd22e",
+    "url": "/todo-app/static/js/runtime-main.e86c861d.js"
+  }
+]);
