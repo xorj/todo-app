a5e6d33557de2305fba38b88245e20932c5f8c8d
xorj <61337156+xorj@users.noreply.github.com>
2020-04-26 20:26:49 -0300

    adding a live version of the app

---
+++ build/precache-manifest.ade77703e2caf2291440dceb4e6243b8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4da70ea0617039882e5b84852bf67d3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0271c92388d8844123ce",
+    "url": "/static/css/main.dffc5d68.chunk.css"
+  },
+  {
+    "revision": "37e170913ffc563d43b4",
+    "url": "/static/js/2.15ae65d4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.15ae65d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0271c92388d8844123ce",
+    "url": "/static/js/main.d4bb87e1.chunk.js"
+  },
+  {
+    "revision": "bcc5b5410d29660f6648",
+    "url": "/static/js/runtime-main.d3a82e94.js"
+  }
+]);
